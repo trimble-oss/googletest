c6d1223b5a6a4c2c82b699df5f1889f7d4f17bbe
Samu Ruuhonen <samu.ruuhonen@trimble.com>
2024-09-06 15:20:36 +0300

    Added local release nuget build support

--- trimble-technology/BuildLocalNuget.bat
+++ trimble-technology/BuildLocalNuget.bat
@@ -1,10 +1,10 @@
-MSBuild GTest.sln /p:Configuration=Debug /p:Platform=x64
-MSBuild GTest.sln /p:Configuration=Release /p:Platform=x64
+MSBuild gtest.sln /p:Configuration=DebugCRT /p:Platform=x64
+MSBuild gtest.sln /p:Configuration=Release /p:Platform=x64
 
 @echo off
 set /p Build=<ver.txt
 set /a Build=%Build%+1
-echo %Build%>ver.txt
+echo %Build% >ver.txt
 
-c:\tools\nuget\nuget.exe pack -Version 0.0.%Build% GTest-v142.nuspec
-c:\tools\nuget\nuget.exe pack -Version 0.0.%Build% GTest-Main-v142.nuspec
+c:\work\nuget.exe pack -Version 1.15.2.%Build% GTest-v142.nuspec
+c:\work\nuget.exe pack -Version 1.15.2.%Build% GTest-Main-v142.nuspec
